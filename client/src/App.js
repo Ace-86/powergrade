35cb5c20b7b5a8ee37749c9254de83a55c573c70
Ace <kronicrespawn@gmail.com>
2023-05-29 11:54:07 -0500

    error with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ClassList from './components/ClassList';
+import GradeHistory from './components/GradeHistory';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> hello World</h1>
-    </div>
-  );
-}
+    <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route path="/" exact>
+        <ClassList />
+      </Route>
+      <Route path="/grade-history/:className">
+        <GradeHistory />
+      </Route>
+    </Routes>
+  </Router>
+);
+};
+
 
 export default App;
