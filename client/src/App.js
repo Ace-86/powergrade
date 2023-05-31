072324b5e461a550318ae2c5244b51db356d2866
Ace <kronicrespawn@gmail.com>
2023-05-30 23:50:47 -0500

    removed axios from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ClassList from './components/ClassList';
 import GradeHistory from './components/GradeHistory';
 import Navbar from './components/Navbar';
+import Axios from 'axios';
 
 function App() {
+
+
+
   return (
     <Router>
       <Navbar />
