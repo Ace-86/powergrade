b04240be25e3b51512433b07266e6973e0777c8e
Ace <kronicrespawn@gmail.com>
2023-05-30 20:53:46 -0500

    seperated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ClassList from './components/ClassList';
 import GradeHistory from './components/GradeHistory';
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/" exact element={<ClassList />} />
-      <Route path="/grade-history/:className"  element={<GradeHistory />} />
-    </Routes>
-  </Router>
-);
-};
-
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<ClassList />} />
+        <Route path="/grade-history/:className" element={<GradeHistory />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
