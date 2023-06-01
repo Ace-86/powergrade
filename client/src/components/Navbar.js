e96ac670c54fdd9d6ad544ccb01ed24295283ed5
Ace <kronicrespawn@gmail.com>
2023-05-31 23:54:13 -0500

    updated home link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,12 +5,7 @@ import '../styles/Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">PowerGrade</div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
+      <Link className="logo" to='/' >PowerGrade</Link>
     </nav>
   );
 }
