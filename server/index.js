8ae9b15a5d985166729654fc0b8fef2ac345787d
Ace <kronicrespawn@gmail.com>
2023-06-01 02:45:26 -0500

    email  connection errors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const app = express();
+const nodemailer = require('nodemailer');
 
 const con = mysql.createConnection({
     user: 'root',
@@ -21,9 +22,38 @@ con.connect(function(err){
     }
 })
 
+const transporter = nodemailer.createTransport({
+    service: 'email-js',
+    auth: {
+      user: 'kronicrespawn@gmail.com',
+      pass: 'Anymean1',
+    },
+  });
+
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/api/send-email', (req, res) => {
+    const { assignment } = req.body;
+  
+    const mailOptions = {
+      from: 'kronicrespawn@gmail.com',
+      to: 'awes86@gmail.com',
+      subject: 'Reminder: Submit Missing Work',
+      text: `Dear Student, \n\nYou have an assignment (${assignment.work}) for the class (${assignment.category}) due on ${assignment.date} that has not been submitted yet. Please submit it as soon as possible.\n\nBest regards,\nYour School`,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Failed to send email:', error);
+        res.status(500).json({ error: 'Failed to send email reminder.' });
+      } else {
+        console.log('Email sent:', info.response);
+        res.status(200).json({ message: 'Email reminder sent successfully!' });
+      }
+    });
+  });
 
 // grabs all data from database;
 app.get('/api/get', (req, res) => {
@@ -34,6 +64,7 @@ app.get('/api/get', (req, res) => {
         res.send(result)
     });
 })
+
 
 app.listen(3001, () =>{
     console.log('running port 3001')
